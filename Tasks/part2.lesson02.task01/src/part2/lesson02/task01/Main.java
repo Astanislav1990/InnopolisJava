c5fcce2ae0170d97b15bf4586c3f37aab67385d7
Astanislav1990 <stanislav.eduardovich@gamail.com>
2020-05-06 13:50:03 +0300

    1st Homework

--- Tasks/part2.lesson02.task01/src/part2/lesson02/task01/Main.java
+++ Tasks/part2.lesson02.task01/src/part2/lesson02/task01/Main.java
@@ -1,8 +1,49 @@
 package part2.lesson02.task01;
 
+class A {
+    public static String s3;
+    public String s4;
+}
+
+
 public class Main {
 
+    public static String s1;
+    //public String s3;
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String s2;
+//        if (s1.equals(s2)) {
+//            System.out.println("Hello world");
+//        } else {
+//            throw new NullPointerException();
+//        }
+
+
+
+        A a = new A();
+//
+//        if (a.s4.length() >= 0){
+//            System.out.println(s1.length());
+//            System.out.println("Hello world");
+//        }else{
+//            System.out.println("");
+//        }
+
+//        String s = "Hello world";
+//        char[] c = s.toCharArray();
+//        System.out.println(c[99]);
+
+//      if(a.s4 == null){
+//          throw new NullPointerException();
+//      }
+
+      try{
+          a.s4.length();
+      }catch (NullPointerException e){
+          throw new ArithmeticException();
+      }
     }
+
 }
